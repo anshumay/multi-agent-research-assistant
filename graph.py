d608c98696360168404a2565d2dc981ec6b03680
Anshumay Rath <anshumayrath@Anshumays-MacBook-Air.local>
2026-05-07 22:54:04 +0530

    Add conditional routing and retry workflow

--- graph.py
+++ graph.py
@@ -16,8 +16,10 @@ from utils.parser import parse_json
 class AgentState(TypedDict):
     query: str
     research: dict
+    research_status: str
     analysis: dict
     report: str
+    retry_count: int
 
 
 # -----------------------------
@@ -32,7 +34,8 @@ def research_node(state: AgentState):
     research = parse_json(research_raw)
 
     return {
-        "research": research
+    "research": research,
+    "retry_count": state.get("retry_count", 0) + 1
     }
 
 
@@ -58,6 +61,34 @@ def writer_node(state: AgentState):
         "report": report
     }
 
+def validate_research_node(state: AgentState):
+
+    print("\n✅ VALIDATION NODE")
+
+    research = state["research"]
+
+    if (
+        isinstance(research, dict)
+        and "facts" in research
+        and len(research["facts"]) > 0
+    ):
+        return {
+            "research_status": "valid"
+        }
+
+    return {
+        "research_status": "invalid"
+    }
+
+def research_router(state: AgentState):
+
+    if state["research_status"] == "valid":
+        return "analysis"
+
+    if state["retry_count"] >= 2:
+        return END
+
+    return "research"
 
 # -----------------------------
 # GRAPH
@@ -66,12 +97,18 @@ def writer_node(state: AgentState):
 builder = StateGraph(AgentState)
 
 builder.add_node("research", research_node)
+builder.add_node("validate_research", validate_research_node)
 builder.add_node("analysis", analysis_node)
 builder.add_node("writer", writer_node)
 
 builder.set_entry_point("research")
 
-builder.add_edge("research", "analysis")
+# builder.add_edge("research", "analysis")
+builder.add_edge("research", "validate_research")
+builder.add_conditional_edges(
+    "validate_research",
+    research_router
+)
 builder.add_edge("analysis", "writer")
 builder.add_edge("writer", END)
 
